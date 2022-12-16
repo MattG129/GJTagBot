e572e2dd2a21d5cdfa545d3022a0aa965a97a9f1
MattG129 <61367937+MattG129@users.noreply.github.com>
2022-12-16 00:46:17 -0500

    Got index to consolelog name, timestamp, and attachment url.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv/config')
 
@@ -6,6 +7,7 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildPresences,
 	],
 })
 
@@ -13,11 +15,43 @@ client.on('ready', () => {
 	console.log('The bot is online');
 })
 
-client.on('MessageCreate', message => {
-	if (message.content === 'Ping') {
-		// message.reply('pong')
-		console.log('Pong')
-	}
+client.on('messageCreate', (message) => {
+	message.attachments.forEach(function(attachment) {
+		console.log(message.author.username);
+		console.log(message.createdTimestamp);
+	    console.log(attachment.url);
+	})
 })
 
 client.login(process.env.TOKEN)
+		// console.log(message.attachments);
+
+		// console.log(message.content);
+
+
+// 	fs.writeFile( 'Test.txt', 'eYo', (err) => {
+//    if (err) throw err;
+//    else{
+//       console.log("The file is updated with the given data")
+//    }
+// })
+
+
+// console.log({'Message': message.content});
+// message.author.username
+// message.attachments
+// message.attachments.forEach(function(attachment) {
+// message.createdTimestamp
+//   console.log(attachment.url);
+// })
+
+
+	// console.log('Pong')
+	// console.log()
+	// console.log(message)	
+	// var utcSeconds = message.createdTimestamp;
+	// var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+	// d.setUTCSeconds(utcSeconds);
+	// console.log(d)
+	// var elo = new Date(message.createdTimestamp)
+	// console.log(elo)
