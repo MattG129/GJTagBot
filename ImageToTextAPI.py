c9afb1044ea30a93241eba670bbfada7240d845d
MattG129 <61367937+MattG129@users.noreply.github.com>
2023-03-25 19:17:10 -0400

    Index will now call a python function to make an api request to an image to text program. While the python script can currently get back the results they are not properly formatted when being sent back to index

---
+++ ImageToTextAPI.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+image_url = sys.argv[1]
+
+img_data = requests.get(image_url).content
+with open('image_name.jpg', 'wb') as handler:
+    handler.write(img_data)
+
+
+api_url = 'https://api.api-ninjas.com/v1/imagetotext'
+# response = requests.get(api_url, headers={'X-Api-Key': 'zUpKbVPc+sDiTEDaEIcj+Q==SCvQDa4VvGJYtCnt'})
+image_file_descriptor = open('image_name.jpg', 'rb')
+# image_file_descriptor = get_image_from_url('https://cdn.discordapp.com/attachments/1051349596883931139/1089295001172852776/image.png')
+files = {'image': image_file_descriptor}
+r = requests.post(api_url, files=files, headers={'X-Api-Key': os.getenv('API_KEY')})
+print(r.json())
+
+
+print(r.json)
+sys.stdout.flush()
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const WOK = require('wokcommands');
-const Tesseract = require("tesseract.js");
 const { Client, GatewayIntentBits } = require('discord.js');
+const jsdom = require('jsdom')
+const dom = new jsdom.JSDOM("")
+const $ = require('jquery')(dom.window)
 require('dotenv/config');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildPresences,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+  ],
 });
 
 client.on('ready', () => {
-	console.log('The bot is online');
+  console.log('The bot is online');
 });
 
 client.on("messageCreate", (message) => {
@@ -24,18 +25,21 @@ client.on("messageCreate", (message) => {
 
       var ImageURL = attachment.proxyURL;
 
-      Tesseract.recognize(
-        ImageURL,
-        "eng",
-      ).then(({ data: { text } }) => {
-        console.log(message.author.username);
-		console.log(message.createdTimestamp);
-    	console.log(attachment.url);
-        console.log(text);
-      });
+      console.log(message)
+      console.log(message.author.username);
+      console.log(message.createdTimestamp);
+      console.log(attachment.url);
+      
+    
     });
   }
 });
 
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',['ImageToTextAPI.py', 'https://cdn.discordapp.com/attachments/1051349596883931139/1089295001172852776/image.png']);
+
+pythonProcess.stdout.on('data', (data) => {
+ console.log(data)
+});
 
 client.login(process.env.TOKEN)
