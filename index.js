daabbe01b9be83b28f2f2cc8c68a1d1216217bce
MattG129 <61367937+MattG129@users.noreply.github.com>
2023-07-30 22:19:57 -0400

    Cleaned up code

--- index.js
+++ index.js
@@ -8,45 +8,34 @@ require('dotenv').config({path: '../.env'})
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildPresences,
   ],
 });
 
 client.on('ready', () => {
-  console.log('The bot is online');
+   console.log('The bot is online');
 });
 
 client.on("messageCreate", (message) => {
-  console.log((['1132806976426999939', '1032196713395527700'].includes(message.channelId)))
-  console.log(1132806976426999939 == message.channelId)
-  if (
-      (message.attachments.size > 0) 
-      && (['1132806976426999939', '1032196713395527700'].includes(message.channelId))
-  ){
-    message.attachments.forEach((attachment) => {
-
-      console.log(message.author.username);
-      console.log(message.createdTimestamp);
-      console.log(attachment.url);
-
-      // See if we can pass the arguments with names so we aren't using magic numbers in the python files
-      const spawn = require("child_process").spawn;
-      var ImageToText = spawn('python',['ImageToTextAPI.py', attachment.url]);
-
-      ImageToText.stdout.on('data', (data) => {
-        
-        // console.log(data.toString('utf8'))
-      
-        var Sheets = spawn('python',['SheetsAPI.py', message.author.username, message.createdTimestamp, attachment.url, data.toString('utf8')]);
-
-        Sheets.stdout.on('data', (data) => {
-            // console.log(data.toString('utf8'))
-        });    
-      });    
-    });
+   if (
+      message.attachments.size > 0
+      && ['1132806976426999939', '1032196713395527700'].includes(message.channelId)
+   ){
+      message.attachments.forEach((attachment) => {
+         // See if we can pass the arguments with names so we aren't using magic numbers in the python files
+         const spawn = require("child_process").spawn;
+         
+         var ImageToText = spawn('python',['ImageToTextAPI.py', attachment.url]);
+         
+         ImageToText.stdout.on('data', (data) => {      
+            var Sheets = spawn('python',['SheetsAPI.py', message.author.username, message.createdTimestamp, attachment.url, data.toString('utf8')]);
+            
+            Sheets.stdout.on('data', (data) => {});    
+         });    
+      });
   }
 });
 
