da9bec7cef20e53a0ee976bda91a603ea91dc247
MattG129 <61367937+MattG129@users.noreply.github.com>
2023-08-10 22:28:13 -0400

    Tag Bot will now use Google Docs API to get list of crew member names.

--- SheetsAPI.py
+++ SheetsAPI.py
@@ -6,57 +6,78 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-try:
-    # I would like to try and add brackets here
-    def GetTags():
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/documents.readonly']
+SHEET_ID = '1oN-BFreDodRP0biTo2Bvp3nxQnvnMblW9fCKfcp3jsA'
+DOCUMENT_ID = '1xh41blzPdXi64NrwkSlNWk1ygUtG5TE6NwJ_PKQc7lk'
 
-        # This should ultimately read from a file in parent directory
-        # Using a scuffed version of Airam's name for now. System for detecting when substrings are 'close enough' to the actual name will be implemented.
-        CrewMembers = ['Matt129', 'Casre', 'NovaPulser', 'Alram', 'Ver.Melakal', 'RedBB', 'Airam', 'suzunya']
+creds = service_account.Credentials.from_service_account_file(
+    filename = '../credentials.json', 
+    scopes   = SCOPES
+)
 
-        MessageID = sys.argv[1]
-        
-        AttachmentID = sys.argv[2]
+def GetNames():  
+    Service = build('docs', 'v1', credentials=creds)
 
-        Tagger = sys.argv[3]
+    # Retrieve the documents contents from the Docs service.
+    Document = Service.documents().get(documentId=DOCUMENT_ID).execute()
 
-        # Will work more on this set up or scrap it if bot can be hosted.
-        PostTime = sys.argv[4] #1682811088360
-        PostTime = f"{datetime.fromtimestamp(int(f'{PostTime}'[:-3]))}"
-
-        # Find a way to make these into hyperlinks
-        ImageURL = f'=HYPERLINK("{sys.argv[5]}", "Image")'
+    Elements = Document.get('body')['content']
     
-        # Combine all text into one string
-        Text = sys.argv[6].replace("\'", "\"")
-        Text = json.loads(Text)
-        Tagged = [[j for j in CrewMembers if j in i['text']] for i in Text]
-        Tagged = list(itertools.chain(*Tagged))
+    Names = []
+    for i in Elements:
+        if 'paragraph' in i:
+            ParagraphElements = i.get('paragraph').get('elements')
+            for j in ParagraphElements:
+                TextRun = j.get('textRun')
+                if TextRun:
+                    Names.append(TextRun.get('content')[:-1])
+    
+    return Names
 
-        TimeStamp = f'{datetime.today().date()} {datetime.today().strftime("%I:%M %p")}'
+def GetTags():    
+    CrewMembers = GetNames()
 
-        Tags = {
-            "values": [
-                [Tagger, i, PostTime, TimeStamp, ImageURL, MessageID, AttachmentID]
-                for i in Tagged
-            ]    
-        }
+    MessageID = sys.argv[1]
+    
+    AttachmentID = sys.argv[2]
 
-        return Tags
+    Tagger = sys.argv[3]
 
-    creds = service_account.Credentials.from_service_account_file(
-        filename='../credentials.json', 
-        scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    # Will work more on this set up or scrap it if bot can be hosted.
+    PostTime = sys.argv[4] #1682811088360
+    PostTime = f"{datetime.fromtimestamp(int(f'{PostTime}'[:-3]))}"
 
+    # Find a way to make these into hyperlinks
+    ImageURL = f'=HYPERLINK("{sys.argv[5]}", "Image")'
+
+    # Combine all text into one string
+    Text = sys.argv[6].replace("\'", "\"")
+    Text = json.loads(Text)
+    Tagged = [[j for j in CrewMembers if j in i['text']] for i in Text]
+    Tagged = list(itertools.chain(*Tagged))
+
+    TimeStamp = f'{datetime.today().date()} {datetime.today().strftime("%I:%M %p")}'
+
+    Tags = {
+        "values": [
+            [Tagger, i, PostTime, TimeStamp, ImageURL, MessageID, AttachmentID]
+            for i in Tagged
+        ]    
+    }
+
+    return Tags
+
+
+try:    
     Service = build('sheets', 'v4', credentials=creds)
 
     # When we append to first row the text becomes bold. This should not be the case.
     request = Service.spreadsheets().values().append(
-        spreadsheetId='1oN-BFreDodRP0biTo2Bvp3nxQnvnMblW9fCKfcp3jsA', 
-        range='Records!A3:E3', 
-        valueInputOption='USER_ENTERED', 
-        insertDataOption='INSERT_ROWS',
-        body=GetTags()
+        spreadsheetId       = SHEET_ID, 
+        range               = 'Records!A3:E3', 
+        valueInputOption    = 'USER_ENTERED', 
+        insertDataOption    = 'INSERT_ROWS',
+        body                = GetTags()
     )
 
     request.execute()
